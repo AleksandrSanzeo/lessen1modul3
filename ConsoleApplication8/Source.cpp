954c8e70c4463097de1a391a656449684baeb330
СанжаровА <@227_5>
2017-12-07 20:59:29 +0600

    Add project files.

---
+++ ConsoleApplication8/Source.cpp
@@ -0,0 +1,144 @@
+#include<stdio.h>
+#include<iostream>
+#include<locale.h>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+#pragma region MyRegion
+	/*int a;
+	cin >> a;
+	int b;
+	cin >> b;
+	int c;
+	c = (a + b) / 2;
+	cout << c;*/
+#pragma endregion
+
+#pragma region MyRegion
+	/*int a;
+	cin >> a;
+	int b;
+	cin >> b;
+	int c;
+	cin >> c;
+	int d;
+	d = (a + b + c) / 3;
+	cout << d;*/
+#pragma endregion
+
+#pragma region MyRegion
+
+	/*cout << "Целое А кратно двум или трем" << endl;
+	int a;
+	cout << "Введите a:";
+	cin >> a;
+	if (a % 2)
+	{
+		cout << "Кратно 2: ";
+	}
+	if (a % 3)
+	{
+		cout << "Кратно 3: ";
+	}*/
+#pragma endregion
+
+#pragma region MyRegion
+
+	/*cout << "чисела  А и В нечетные" << endl;
+	int a, b;
+	cout << "Введите a: ";
+	cin >> a;
+	cout << "Введите b: ";
+	cin >> b;
+	if ((a % 2) > 0 && (b % 2) > 0)
+	{
+		cout << "правда: " << endl;
+	}
+	else
+	{
+		cout << "лож: " << endl;
+	}*/
+
+#pragma endregion
+
+#pragma region MyRegion
+
+	/*cout << "число А является трехзначным" << endl;
+	int a;
+	cout << "Введите a: ";
+	cin >> a;
+
+	if (a > 111)
+	{
+		cout << "правда: " << endl;
+	}
+	else
+	{
+		cout << "лож: " << endl;
+	}*/
+
+#pragma endregion
+
+#pragma region MyRegion
+
+	/*cout << "одно из чисел А, В и С, меньше 45." << endl;
+	int a, b, c;
+	cout << "Введите a: ";
+	cin >> a;
+	cout << "Введите b: ";
+	cin >> b;
+	cout << "Введите c: ";
+	cin >> c;
+	if ((a < 45) || (b < 45) || (c < 45))
+	{
+		cout << "правда: " << endl;
+	}
+	else
+	{
+		cout << "лож: " << endl;
+	}*/
+#pragma endregion
+
+#pragma region MyRegion
+
+	/*cout << "целое А не кратно трем и оканчивается нулем." << endl;
+	int A;
+	cout << "Введите A: ";
+	cin >> A;
+	if ((A % 3) > 0)
+	{
+	cout << "правда: " << endl;
+	}
+	else
+	{
+		cout << "лож: " << endl;
+	}*/
+
+#pragma endregion
+
+#pragma region MyRegion
+
+	/*cout << "одно из чисел X, Y и Z кратно пяти" << endl;
+	int X;
+	cout << "Введите X:";
+	cin >> X;
+	int Y;
+	cout << "Введите Y:";
+	cin >> Y;
+	int Z;
+	cout << "Введите Z:";
+	cin >> Z;
+	if ((X % 5) == 0 || (Y % 5) == 0 || (Z % 5) == 0)
+	{
+		cout << "Правда: ";
+	}
+	else
+	{
+		cout << "Лож: ";
+	}*/
+		
+#pragma endregion
+
+}
